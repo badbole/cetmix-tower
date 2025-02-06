d99bc7be28e3e87efb2f5394cefdb970f68be1df
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-02-06 15:52:11 +0100

    [IMP] cetmix_tower_server Command access
    Update access rules for Command.

--- cetmix_tower_server/tests/__init__.py
+++ cetmix_tower_server/tests/__init__.py
@@ -1,11 +1,11 @@
 from . import test_server
-
-# from . import test_variable
-# from . import test_command
-# from . import test_key
-# from . import test_plan
+from . import test_command
 from . import test_file
 from . import test_file_template
+from . import test_plan
+# from . import test_variable
+# from . import test_key
+# from . import test_plan
 # from . import test_plan_log
 # from . import test_command_wizard
 # from . import test_server_log
