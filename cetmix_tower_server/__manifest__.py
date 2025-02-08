10bd4f698c85d5a3b382adcc5f49f8209a67a460
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-02-08 23:35:07 +0100

    Revert demo data
    TODO: split demo data into multiple files.

--- cetmix_tower_server/__manifest__.py
+++ cetmix_tower_server/__manifest__.py
@@ -56,8 +56,6 @@
         "views/menuitems.xml",
     ],
     "demo": [
-        "demo/demo_data_generic.xml",
-        "demo/server_template_demo.xml",
-        "demo/flight_plan_demo.xml",
+        "demo/demo_data.xml",
     ],
 }
